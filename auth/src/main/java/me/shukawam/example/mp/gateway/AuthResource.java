074d29ddc5fb29cb2c68556e67519ee899ed2ca8
shukawam <shukawam@gmail.com>
2021-06-20 16:50:40 +0900

    Edit auth resource

--- auth/src/main/java/me/shukawam/example/mp/gateway/AuthResource.java
+++ auth/src/main/java/me/shukawam/example/mp/gateway/AuthResource.java
@@ -1,29 +1,48 @@
 package me.shukawam.example.mp.gateway;
 
+import io.helidon.config.Config;
 import io.helidon.security.SecurityContext;
 import io.helidon.security.annotations.Authenticated;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.Collections;
+import java.util.Optional;
 
 @Path("auth")
 public class AuthResource {
     private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
+    private final String cookieName;
+
+    public AuthResource() {
+        Config config = Config.create();
+        cookieName = config.get("security.providers.oidc.cookie-name").asString().orElse("JSESSIONID");
+    }
 
     @GET
     @Path("login")
     @Authenticated
     @Produces(MediaType.APPLICATION_JSON)
-    public JsonObject login(@Context SecurityContext securityContext) {
-        return JSON.createObjectBuilder()
-                .add("message", String.format("Hello %s!", securityContext.userName()))
+    public JsonObject login(@Context SecurityContext securityContext, @Context ContainerRequestContext containerRequestContext) {
+        return JSON.createObjectBuilder().add("access_token", getAccessToken(containerRequestContext))
                 .build();
+    }
+
+    private String getAccessToken(ContainerRequestContext containerRequestContext) {
+        var cookie = containerRequestContext.getCookies().get(cookieName);
+        if (Optional.ofNullable(cookie).isPresent()) {
+            return cookie.getValue();
+        } else {
+            throw new RuntimeException("No cookie is existing.");
+        }
     }
 }
